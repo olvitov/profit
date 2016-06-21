63279162fa193a9024c8eed7f29629d14ad25ae7
olvit <olvitov@gmail.com>
2016-06-21 14:43:17 +0300

    Буфер Вывода

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 require  __DIR__ . '/autoload.php';
 
-$users = \App\Models\User::findAll();
-include __DIR__ . '/App/templates/index.php';
+$user = new \App\Models\User();
+$user->getEmail();
+
+
+
+$view = new \App\View();
+$view->title = 'Мой сайт!';
+$view->users = \App\Models\User::findAll();
+
+echo $view->render( __DIR__ . '/App/templates/index.php');
+
+
 
 
 
